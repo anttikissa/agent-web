aa643eadb53dedeabea36d1b851b8e62fc6e487e
speksi <speksi@cod.nodeta.fi>
2013-01-27 18:43:26 +0200

    Center the banner, clean up stuff.

--- index.php
+++ index.php
@@ -9,85 +9,88 @@
 </header>
 
 <div id='main'>
-	<nav>
-		<ul>
-			<li><a>Etusivu</a>
-				<ul>
-					<li><a>Menu</a>
-					<li><a>Toinen</a>
-				</ul>
-			<li class='current'><a>Agent</a>
-				<ul>
-					<li class='current'><a>Tarina</a>
-					<li><a>Hahmot</a>
-				</ul>
-			</li>
-			<li><a>Liput</a>
-				<ul>
-					<li><a>Menu</a>
-					<li><a>Toinen</a>
-				</ul>
-			<li><a>Tekijät</a>
-				<ul>
-					<li><a>Osa-alue</a>
-					<li><a>Toinen</a>
-					<li><a>Toinen</a>
-					<li><a>Toinen</a>
-				</ul>
-			<li><a>Sponsorit</a>
-				<ul>
-					<li><a>Sponsori</a>
-					<li><a>kolmas</a>
-					<li><a>kolmas</a>
-				</ul>
-			<li><a>Mikä speksi?</a>
-				<ul>
-					<li><a>Mikä</a>
-					<li><a>on</a>
-				</ul>
-		</ul>
-	</nav>
 
-	<article>
-		<h2>Lorem ipsum dolor</h2>
-		<p>Lorem ipsum dolor sit amet, diam et augue parturient, suscipit id orci 
-	ridiculus. Volutpat amet, eu wisi nibh. Luctus sit. Orci nulla wisi bibendum 
-	<a>sagittis quisque</a>, libero sodales mauris ipsum tincidunt auctor, erat amet. 
-	Tellus egestas maecenas, penatibus sed sed mi nec vitae, convallis ut, ante 
-	magna.
+	<div id='content'>
+		<nav>
+			<ul>
+				<li><a>Etusivu</a>
+					<ul>
+						<li><a>Menu</a>
+						<li><a>Toinen</a>
+					</ul>
+				<li class='current'><a>Agent</a>
+					<ul>
+						<li class='current'><a>Tarina</a>
+						<li><a>Hahmot</a>
+					</ul>
+				</li>
+				<li><a>Liput</a>
+					<ul>
+						<li><a>Menu</a>
+						<li><a>Toinen</a>
+					</ul>
+				<li><a>Tekijät</a>
+					<ul>
+						<li><a>Osa-alue</a>
+						<li><a>Toinen</a>
+						<li><a>Toinen</a>
+						<li><a>Toinen</a>
+					</ul>
+				<li><a>Sponsorit</a>
+					<ul>
+						<li><a>Sponsori</a>
+						<li><a>kolmas</a>
+						<li><a>kolmas</a>
+					</ul>
+				<li><a>Mikä speksi?</a>
+					<ul>
+						<li><a>Mikä</a>
+						<li><a>on</a>
+					</ul>
+			</ul>
+		</nav>
 
-		<h2>Vel aenean nibh</h2>
-		<p>Vel aenean nibh morbi, eu arcu vehicula, aenean ornare mauris metus 
-	aliquam fusce, eget sapien sed, pede integer metus. Magna dolor enim wisi lorem 
-	massa in, nulla odio vitae blandit sed, quam pede, cras metus non pellentesque 
-	ante sed. <a>Purus ut massa</a> rutrum posuere egestas ac, quis tincidunt bibendum 
-	turpis tellus sit augue, nulla aliquam vel, est vestibulum nibh quam 
-	condimentum suspendisse. Non commodo ipsum habitasse, rhoncus vitae erat pede 
-	odio, quisque risus. Metus enim pretium nec, quisque volutpat phasellus in, 
-	amet tempus pede orci eleifend cum arcu, ligula faucibus, risus feugiat nunc. 
-	Aliquet suspendisse fermentum at adipiscing morbi metus, wisi sapien malesuada 
-	ultricies non amet sit. Volutpat quam velit vitae proin mauris, nam donec 
-	molestias nulla pellentesque et fusce, ut dignissim elit, donec ipsa semper.
+		<article>
+			<h2>Lorem ipsum dolor</h2>
+			<p>Lorem ipsum dolor sit amet, diam et augue parturient, suscipit id orci 
+		ridiculus. Volutpat amet, eu wisi nibh. Luctus sit. Orci nulla wisi bibendum 
+		<a>sagittis quisque</a>, libero sodales mauris ipsum tincidunt auctor, erat amet. 
+		Tellus egestas maecenas, penatibus sed sed mi nec vitae, convallis ut, ante 
+		magna.
 
-		<h2>Felis velit sed</h2>
-		<p>Felis velit sed, in amet non eiusmod nibh leo augue. Dictumst aliquam metus 
-	senectus, nisl pede sit sed faucibus justo. Eros id vitae, integer dictum orci 
-	orci, sapien felis quam et blandit. Convallis vel nec. Donec erat nulla 
-	sociosqu odio erat. Donec etiam sed natus, turpis commodo euismod facilisi orci 
-	dolor nunc, eleifend ipsum suscipit cursus ornare, <a>sociis</a> odio mus 
-	aliquam sit nibh pharetra. Tellus sit lectus risus tempus quam, amet non. Eu 
-	ac, donec donec, adipisicing nunc proin aptent pretium cras vivamus, leo vitae. 
-	Lacus duis eu vestibulum ante auctor, ultrices sapien, donec sit enim posuere 
-	proin libero, et nunc in leo augue id vehicula
+			<h2>Vel aenean nibh</h2>
+			<p>Vel aenean nibh morbi, eu arcu vehicula, aenean ornare mauris metus 
+		aliquam fusce, eget sapien sed, pede integer metus. Magna dolor enim wisi lorem 
+		massa in, nulla odio vitae blandit sed, quam pede, cras metus non pellentesque 
+		ante sed. <a>Purus ut massa</a> rutrum posuere egestas ac, quis tincidunt bibendum 
+		turpis tellus sit augue, nulla aliquam vel, est vestibulum nibh quam 
+		condimentum suspendisse. Non commodo ipsum habitasse, rhoncus vitae erat pede 
+		odio, quisque risus. Metus enim pretium nec, quisque volutpat phasellus in, 
+		amet tempus pede orci eleifend cum arcu, ligula faucibus, risus feugiat nunc. 
+		Aliquet suspendisse fermentum at adipiscing morbi metus, wisi sapien malesuada 
+		ultricies non amet sit. Volutpat quam velit vitae proin mauris, nam donec 
+		molestias nulla pellentesque et fusce, ut dignissim elit, donec ipsa semper.
 
-	</article>
+			<h2>Felis velit sed</h2>
+			<p>Felis velit sed, in amet non eiusmod nibh leo augue. Dictumst aliquam metus 
+		senectus, nisl pede sit sed faucibus justo. Eros id vitae, integer dictum orci 
+		orci, sapien felis quam et blandit. Convallis vel nec. Donec erat nulla 
+		sociosqu odio erat. Donec etiam sed natus, turpis commodo euismod facilisi orci 
+		dolor nunc, eleifend ipsum suscipit cursus ornare, <a>sociis</a> odio mus 
+		aliquam sit nibh pharetra. Tellus sit lectus risus tempus quam, amet non. Eu 
+		ac, donec donec, adipisicing nunc proin aptent pretium cras vivamus, leo vitae. 
+		Lacus duis eu vestibulum ante auctor, ultrices sapien, donec sit enim posuere 
+		proin libero, et nunc in leo augue id vehicula
 
-	<div style='clear: both'></div>
+		</article>
+
+		<div style='clear: both'></div>
+	</div>
+	<footer>
+		<a>teekkarispeksi.fi</a>
+	</footer>
 </div>
 
-<footer>
-	<a>teekkarispeksi.fi</a>
-</footer>
 
 <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'></script>
 <script src='js/script.js'></script>
