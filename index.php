2cae88c5a3989dec0ab68b4d40c16407c975c953
speksi <speksi@cod.nodeta.fi>
2013-01-27 02:29:10 +0200

    Use styles, add some lorem ipsum.

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
 <?php header('Content-Type: text/html; charset=utf-8') ?>
 <!doctype html>
 <meta charset='utf-8'>
+<link rel='stylesheet' href='css/style.css'>
 <title>Agent</title>
 
-Hi!
+<header>
+</header>
+
+<nav>
+	<ul>
+		<li>Item1</li>
+		<li>Item2</li>
+	</ul>
+</nav>
+
+<article>
+<p>Lorem ipsum dolor sit amet, diam et augue parturient, suscipit id orci ridiculus. Volutpat amet, eu wisi nibh. Luctus sit. Orci nulla wisi bibendum sagittis quisque, libero sodales mauris ipsum tincidunt auctor, erat amet. Tellus egestas maecenas, penatibus sed sed mi nec vitae, convallis ut, ante magna.
+
+<p>Vel aenean nibh morbi, eu arcu vehicula, aenean ornare mauris metus aliquam fusce, eget sapien sed, pede integer metus. Magna dolor enim wisi lorem massa in, nulla odio vitae blandit sed, quam pede, cras metus non pellentesque ante sed. Purus ut massa rutrum posuere egestas ac, quis tincidunt bibendum turpis tellus sit augue, nulla aliquam vel, est vestibulum nibh quam condimentum suspendisse. Non commodo ipsum habitasse, rhoncus vitae erat pede odio, quisque risus. Metus enim pretium nec, quisque volutpat phasellus in, amet tempus pede orci eleifend cum arcu, ligula faucibus, risus feugiat nunc. Aliquet suspendisse fermentum at adipiscing morbi metus, wisi sapien malesuada ultricies non amet sit. Volutpat quam velit vitae proin mauris, nam donec molestias nulla pellentesque et fusce, ut dignissim elit, donec ipsa semper.
+
+<p>Felis velit sed, in amet non eiusmod nibh leo augue. Dictumst aliquam metus senectus, nisl pede sit sed faucibus justo. Eros id vitae, integer dictum orci orci, sapien felis quam et blandit. Convallis vel nec. Donec erat nulla sociosqu odio erat. Donec etiam sed natus, turpis commodo euismod facilisi orci dolor nunc, eleifend ipsum suscipit cursus ornare, sociis odio mus aliquam sit nibh pharetra. Tellus sit lectus risus tempus quam, amet non. Eu ac, donec donec, adipisicing nunc proin aptent pretium cras vivamus, leo vitae. Lacus duis eu vestibulum ante auctor, ultrices sapien, donec sit enim posuere proin libero, et nunc in leo augue id vehicula
+
+</article>
+
+<footer>
+</footer>
+
+
