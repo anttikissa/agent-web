f06edb421367292379e03773af631e929a18cdb0
Antti Sykäri <antti.sykari@gmail.com>
2013-02-02 22:58:38 +0200

    Use actual content on each page.

--- index.php
+++ index.php
@@ -74,41 +74,12 @@ $pages = array(
 		print("</li>");
 	}
 ?>
-
 			</ul>
 		</nav>
 
 		<article>
-			<h2>Lorem ipsum dolor</h2>
-			<p>Lorem ipsum dolor sit amet, diam et augue parturient, suscipit id orci 
-		ridiculus. Volutpat amet, eu wisi nibh. Luctus sit. Orci nulla wisi bibendum 
-		<a>sagittis quisque</a>, libero sodales mauris ipsum tincidunt auctor, erat amet. 
-		Tellus egestas maecenas, penatibus sed sed mi nec vitae, convallis ut, ante 
-		magna.
 
-			<h2>Vel aenean nibh</h2>
-			<p>Vel aenean nibh morbi, eu arcu vehicula, aenean ornare mauris metus 
-		aliquam fusce, eget sapien sed, pede integer metus. Magna dolor enim wisi lorem 
-		massa in, nulla odio vitae blandit sed, quam pede, cras metus non pellentesque 
-		ante sed. <a>Purus ut massa</a> rutrum posuere egestas ac, quis tincidunt bibendum 
-		turpis tellus sit augue, nulla aliquam vel, est vestibulum nibh quam 
-		condimentum suspendisse. Non commodo ipsum habitasse, rhoncus vitae erat pede 
-		odio, quisque risus. Metus enim pretium nec, quisque volutpat phasellus in, 
-		amet tempus pede orci eleifend cum arcu, ligula faucibus, risus feugiat nunc. 
-		Aliquet suspendisse fermentum at adipiscing morbi metus, wisi sapien malesuada 
-		ultricies non amet sit. Volutpat quam velit vitae proin mauris, nam donec 
-		molestias nulla pellentesque et fusce, ut dignissim elit, donec ipsa semper.
-
-			<h2>Felis velit sed</h2>
-			<p>Felis velit sed, in amet non eiusmod nibh leo augue. Dictumst aliquam metus 
-		senectus, nisl pede sit sed faucibus justo. Eros id vitae, integer dictum orci 
-		orci, sapien felis quam et blandit. Convallis vel nec. Donec erat nulla 
-		sociosqu odio erat. Donec etiam sed natus, turpis commodo euismod facilisi orci 
-		dolor nunc, eleifend ipsum suscipit cursus ornare, <a>sociis</a> odio mus 
-		aliquam sit nibh pharetra. Tellus sit lectus risus tempus quam, amet non. Eu 
-		ac, donec donec, adipisicing nunc proin aptent pretium cras vivamus, leo vitae. 
-		Lacus duis eu vestibulum ante auctor, ultrices sapien, donec sit enim posuere 
-		proin libero, et nunc in leo augue id vehicula
+<?php include($current_page . '/content.html'); ?>
 
 		</article>
 
