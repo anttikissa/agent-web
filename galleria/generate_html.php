66ca8bed03a8eeccbe1aa37a5779341a68b295d8
Pekko Lipsanen <pekko.lipsanen@iki.fi>
2013-02-05 23:46:58 +0200

    gallery

--- galleria/generate_html.php
+++ galleria/generate_html.php
@@ -1,7 +1,17 @@
 <?php
 foreach(glob('img/galleria/thumb/*') as $f) {
+	$suffix = substr($f, strrpos($f, "."));
 	$base = basename($f);
-	echo "<a rel='galleria' class='fancybox galleria' href='img/galleria/large/".$base."'>";
+	$base_nosuffix = basename($f, $suffix);
+
+	echo "<a";
+
+	$caption_file = "content/galleria/caption/".$base_nosuffix.".txt";
+	if(is_file($caption_file)) {
+		echo " title='".file_get_contents($caption_file);
+	}
+
+	echo " rel='galleria' class='fancybox galleria' href='img/galleria/large/".$base."'>";
 	echo "<img src='img/galleria/thumb/".$base."'>";
 	echo "</a>\n\n";
 }
